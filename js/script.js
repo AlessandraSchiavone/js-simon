c92537d51a9c6834a171ae44c2650fbaa0817a79
AlessandraSchiavone <alessandraschiavone31@gmail.com>
2021-05-06 15:15:59 +0200

    Esercizio completo

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// Un alert() espone 5 numeri generati casualmente.
+// Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+/*----------FUNCTIONS--------*/
+function numberRandom(min,max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function isInArray(element, array){
+    for(var i=0; i< array.length;i++){
+        if(element == array[i]){
+            return true;
+        }
+    }
+    return false;
+}
+// Un alert() espone 5 numeri generati casualmente.
+var array5RandomPC = [];
+while(array5RandomPC.length < 5){
+    var randomNumber = numberRandom(1,100);
+    if(!isInArray(randomNumber,array5RandomPC)){
+        array5RandomPC.push(randomNumber);
+    }
+}
+// console.log(array5RandomPC);
+
+alert("Memorizza questi numeri:\n"+ array5RandomPC);
+// Da li parte un timer di 30 secondi.
+var inizioTimer=5;
+var timer = setInterval( function(){
+    console.log(inizioTimer);
+    if(inizioTimer == 0){
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+        clearInterval(timer);
+        console.log("Fine tempo!");
+        var array5Utente = [];
+    do{
+        numeroUtente = parseInt(prompt("Ora Tocca a te\nInserisci un numero:"));
+        if(isNaN(numeroUtente)){
+            console.log("Errore non hai inserito un numero!");
+        }else if (!isInArray(numeroUtente,array5Utente)){
+            array5Utente.push(numeroUtente);
+        } else{
+            console.log("Errore hai già inserito questo numero!");
+        }
+    }while(array5Utente.length < 5);
+    // console.log(array5Utente);
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+    var k=0;
+    var numeriIndovinati= [];
+    for(i=0;i<5;i++){
+        var control = isInArray(array5Utente[i], array5RandomPC);    
+        if(control){     
+            numeriIndovinati.push(array5Utente[i]);
+            // console.log("Il numero indovinato è:",array5Utente[i]);
+            k++;
+            // console.log(k);
+        }
+    }
+    console.log("I numeri del gioco sono:", array5RandomPC);
+    console.log("I numeri inseriti dall'utente sono:", array5Utente);
+    if(k==0){
+        console.log("Non hai indovinato nessun numero!")
+    }else if(k==1){  
+        console.log("Hai indovinato "+ k + " numero");
+        console.log("Il numero indovinato è: ", numeriIndovinati)
+    }else{
+        console.log("Hai indovinato  "+ k + " numeri");
+        console.log("I numeri indovinati sono: ", numeriIndovinati)
+    }
+    }else{
+        inizioTimer--;
+    }
+},1000);
+
